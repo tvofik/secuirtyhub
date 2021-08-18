8dcabc36309c1e9c4b3bd0838d9042e1a869e126
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2021-08-18 19:40:27 -0400

    builder

--- builder/main.tf
+++ builder/main.tf
@@ -1,15 +1,5 @@
 resource "local_file" "foo" {
   content  = templatefile("${path.module}/build.tpl", { keys = var.name_providers })
-  filename = var.filename
-}
-
-variable "name_providers" {
-  type        = list(string)
-  description = "name of providers "
-  default     = ["a", "b", "c"]
-}
-variable "filename" {}
-
-provider "aws" {
-  region = "us-east-1"
+  filename = "${path.root}/build.tf"
+  # filename = var.filename
 }
---
+++ builder/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "filename" {}
+variable "name_providers" {
+  type        = list(string)
+  description = "name of providers "
+  default     = ["a", "b", "c"]
+}
