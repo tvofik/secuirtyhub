42e4039b78cd6abdb19754b8d519ce8f8410901e
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2021-08-18 20:36:42 -0400

    output-1

--- builder/variables.tf
+++ builder/variables.tf
@@ -2,7 +2,7 @@ variable "region" {
   type    = string
   default = "us-east-1"
 }
-variable "filename" {}
+# variable "filename" {}
 variable "name_providers" {
   type        = list(string)
   description = "name of providers "
